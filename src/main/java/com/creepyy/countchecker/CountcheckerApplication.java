469130a851dfa0a7b6e5de4257b9085595874690
panpanYuya <yuya@sanbakaraisu.com>
2023-08-05 17:58:41 +0900

    first-commit

---
+++ src/main/java/com/creepyy/countchecker/CountcheckerApplication.java
@@ -0,0 +1,13 @@
+package com.creepyy.countchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountcheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountcheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/creepyy/countchecker/CountcheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creepyy.countchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountcheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
