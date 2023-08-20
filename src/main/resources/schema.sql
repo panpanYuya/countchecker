95e9f21dcee2874bfda839a67052dbc53425f956
panpanYuya <yuya@sanbakaraisu.com>
2023-08-20 15:33:10 +0900

    API仕様書と初期実行のデータベースを修正

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,59 @@
 DROP TABLE IF EXISTS constipation;
+DROP TABLE IF EXISTS m_status;
+DROP TABLE IF EXISTS m_color;
+DROP TABLE IF EXISTS m_smell;
+DROP TABLE IF EXISTS m_quantity;
+DROP TABLE IF EXISTS m_refresh_feel;
 
 CREATE TABLE IF NOT EXISTS constipation (
-  id MEDIUMINT NOT NULL AUTO_INCREMENT,
-  user_id VARCHAR(100) NOT NULL UNIQUE,
-  created_at DATETIME NOT NULL,
-  updated_at DATETIME NOT NULL,
-  PRIMARY KEY(id,user_id)
+    id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    user_id int NOT NULL ,
+    status_id int NOT NULL ,
+    color_id int NOT NULL ,
+    smell_id int NOT NULL ,
+    quantity_id int NOT NULL ,
+    refresh_feel_id int NOT NULL ,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS m_status (
+    id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    status  VARCHAR(100) NOT NULL ,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS m_color (
+    id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    color VARCHAR(100) NOT NULL ,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS m_smell (
+    id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    smell VARCHAR(100) NOT NULL ,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS m_quantity (
+    id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    quantity VARCHAR(100) NOT NULL ,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS m_refresh_feel (
+    id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    refresh_feel VARCHAR(100) NOT NULL ,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    PRIMARY KEY(id)
 );
